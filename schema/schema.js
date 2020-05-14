09f14b70891eb72723e6610e0f45140bd3dc1316
Vladyslav Kondratiev <vladkon@Vladyslavs-Mac-mini.local>
2020-05-14 23:05:30 +0300

    nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -2,13 +2,35 @@ const axios = require("axios");
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-// schema
+// schemas
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        /**
+         * parentValue is a user, with populated companyId property
+         * by which you can grab company data
+         */
+
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
@@ -25,7 +47,7 @@ const RootQuery = new GraphQLObjectType({
       // (1)
       type: UserType, // (3)
       args: { id: { type: GraphQLString } }, // (2)
-      async resolve(parentValue, args) {
+      resolve(parentValue, args) {
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
           .then((res) => res.data);
